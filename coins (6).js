ea7fb430331b598cd7706782928db7b73e81ea9e
zeev moyal <ZM0548407450@gmail.com>
2025-06-17 23:24:36 +0300

    Delete coins (6).js

--- coins (6).js
+++
@@ -1,7 +0,0 @@
-let coins = parseInt(localStorage.getItem("quizCoins")) || 0;
-
-function updateCoins(timeTaken) {
-  const earned = Math.max(0, 20 - timeTaken);
-  coins += earned;
-  localStorage.setItem("quizCoins", coins);
-}
